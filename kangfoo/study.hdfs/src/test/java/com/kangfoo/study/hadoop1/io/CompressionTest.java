2a3f1a791fc0fddc4cbb7ab4b5cf9c01c55e58e5
kangfoo <baso4233@126.com>
2013-12-22 18:23:30 +0800

    default,gzip codec

--- kangfoo/study.hdfs/src/test/java/com/kangfoo/study/hadoop1/io/CompressionTest.java
+++ kangfoo/study.hdfs/src/test/java/com/kangfoo/study/hadoop1/io/CompressionTest.java
@@ -1,7 +1,5 @@
 package com.kangfoo.study.hadoop1.io;
 
-import static org.junit.Assert.*;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -18,14 +16,22 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 /**
+ * 
+ * 
+ * export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hadoop/env/hadoop/lib/native/Linux-amd64-64:/usr/local/lib
+ * mvn test -Dtest=com.kangfoo.study.hadoop1.io.CompressionTest
+ * 
  * @Date 2013-12-11 17:21
- * @author kangfoo-mac
+ * @author kangfoo-mac 
  *
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CompressionTest {
 
 	private String synappyCodeClazz = "org.apache.hadoop.io.compress.SnappyCodec";
+	private String defaultCodecClazz ="org.apache.hadoop.io.compress.DefaultCodec";
+	private String gzipCodecClazz = "org.apache.hadoop.io.compress.GzipCodec";
+	
 	private static Configuration conf = null;
 	
 	@BeforeClass
@@ -33,9 +39,29 @@ public class CompressionTest {
 		conf = new Configuration();
 	}
 
+	/**
+	 *  
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
 	@Test
 	public void test1_SynappyCompressionCodec() throws ClassNotFoundException, IOException {
-		Class<?> clzz = Class.forName(synappyCodeClazz);
+		baseCompressionCodec(synappyCodeClazz);
+	}
+	
+	@Test
+	public void test2_DefaultCompressionCodec() throws ClassNotFoundException, IOException {
+		baseCompressionCodec(defaultCodecClazz);
+	}
+	
+	@Test
+	public void test3_GzipCompressionCodec() throws ClassNotFoundException, IOException {
+		baseCompressionCodec(gzipCodecClazz);
+	}
+
+	private void baseCompressionCodec(String clazz) throws ClassNotFoundException,
+			FileNotFoundException, IOException {
+		Class<?> clzz = Class.forName(clazz);
 		CompressionCodec codec = (CompressionCodec)ReflectionUtils.newInstance(clzz, conf);
 		String inputFile = "/home/hadoop/env/hadoop/docs/releasenotes.html";
 		String outFile = inputFile + codec.getDefaultExtension();
